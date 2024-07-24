b3dbdd1ac0a48b9b441c1b1656c6f2b371142750
janak-ojha <janakrajojha66@gmail.com>
2024-07-24 23:44:22 +0545

    bug removed

--- api/index.js
+++ api/index.js
@@ -6,44 +6,45 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-const multer =require("multer");
+const multer = require("multer");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
-
-dotenv.config()
+dotenv.config();
 app.use(express.json());
-app.use("/images",express.static(path.join(__dirname,"/images")))
 
+app.get("/", (req, res) => {
+    res.send("Api is running Successfully");
+});
+
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    
 })
-.then(console.log("connected to MongoDB"))
-.catch((err) => console.log(err))
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.log(err));
 
-const storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"images");
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
     },
-    filename:(req,file,cb) =>{
-        cb(null,req.body.name);
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
     },
 });
 
-const upload= multer({ storage: storage});
-app.post("/api/upload",upload.single("file"),(req,res) => {
-    res.status(300).json("file have been uploaded");
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
 });
 
-app.use("/api/auth",authRoute);
-app.use("/api/users",userRoute);
-app.use("/api/posts",postRoute);
-app.use("/api/categories",categoryRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
-
-app.listen(PORT,() =>{
-    console.log("backend is running")
+app.listen(PORT, () => {
+    console.log(`Backend is running on port ${PORT}`);
 });
